a3424c8bbfcbf13756c971a5ff2b55de4c99dd71
Shanzida-Rifa <rifashanzida@gmail.com>
2022-09-28 12:50:51 +0600

    bank

--- js/withdraw.js
+++ js/withdraw.js
@@ -12,21 +12,32 @@ document.getElementById('btn-withdraw').addEventListener('click', function () {
     const withdrawField = document.getElementById('withdraw-field');
     const newWithdrawAmountString = withdrawField.value;
     const newWithdrawAmount = parseFloat(newWithdrawAmountString);
+    withdrawField.value = '';
+
+    if (isNaN(newWithdrawAmount)) {
+        alert('please provide a valid number');
+        return;
+    }
 
     const withdrawTotalElement = document.getElementById('withdraw-total');
     const previousWithdrawTotalString = withdrawTotalElement.innerText;
     const previousWithdrawTotal = parseFloat(previousWithdrawTotalString);
 
-    const currentWithdrawTotal = previousWithdrawTotal + newWithdrawAmount;
-    withdrawTotalElement.innerText = currentWithdrawTotal;
-
     const balanceTotalElement = document.getElementById('balance-total');
     const previousBalanceTotalString = balanceTotalElement.innerText;
     const previousBalanceTotal = parseFloat(previousBalanceTotalString);
+
+
+    if (newWithdrawAmount > previousBalanceTotal) {
+        alert('No sufficient balance for withdraw');
+        return;
+    }
+    const currentWithdrawTotal = previousWithdrawTotal + newWithdrawAmount;
+    withdrawTotalElement.innerText = currentWithdrawTotal;
 
     const newBalanceTotal = previousBalanceTotal - newWithdrawAmount;
     balanceTotalElement.innerText = newBalanceTotal;
 
 
-    withdrawField.value = '';
+
 })
